118b3ff813f919eb436d58b9ed7e2243fd396242
Xenrathe <126534032+Xenrathe@users.noreply.github.com>
2023-11-13 10:32:16 -0500

    Add flights index view and flights controller index functionality

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,7 +1,30 @@
 class FlightsController < ApplicationController
 
   def index
-    @flights = Flight.all
-  end
+    @flights = Flight.all.order(takeoff: :asc)
+    @spaceport_choices = Spaceport.all.map{ |sp| [ sp.full_name, sp.id ]}
 
+    # Filter: based on departure spaceport
+    if params[:departure_spaceport].present? && !params[:departure_spaceport].nil?
+      @flights = @flights.where(departure_spaceport_id: params[:departure_spaceport])
+    end
+
+     # Filter: based on arrival spaceport
+    if params[:arrival_spaceport].present? && !params[:arrival_spaceport].nil?
+      @flights = @flights.where(arrival_spaceport_id: params[:arrival_spaceport])
+    end
+
+    if params[:takeoff_monthday].present? && params[:takeoff_year].present?
+      year = params[:takeoff_year]
+      if params[:takeoff_monthday] == "Any"
+        @flights = @flights.where("EXTRACT(YEAR FROM takeoff) = ?", year)
+      else
+        month, day = params[:takeoff_monthday].split('/')
+        @flights = @flights.where("EXTRACT(MONTH FROM takeoff) = ? AND EXTRACT(DAY FROM takeoff) = ? AND EXTRACT(YEAR FROM takeoff) = ?", month, day, year)
+      end
+    end
+
+    @monthday_choices = @flights.map { |flt| [ flt.takeoff.strftime("%m/%d")] }.uniq
+    @year_choices = @flights.map { |flt| [ flt.takeoff.strftime("%Y"), flt.takeoff.year ] }.uniq
+  end
 end
