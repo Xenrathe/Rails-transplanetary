fb4f525a88ffe44ad84e48fecca402483519d353
Xenrathe <126534032+Xenrathe@users.noreply.github.com>
2023-11-18 17:32:46 -0500

    Add bootstrap, switch from importmaps to ESM, begin styling flight index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :bookings, only: [:new, :create, :show]
+  resources :bookings, only: [:new, :create, :show, :index]
   resources :flights, only: [:index]
+  resources :spaceports, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
